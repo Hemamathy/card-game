e1898fada60db391b78ea877795375626cad2b49
Hema Mathy <hema.bernatsha@gmail.com>
2026-03-30 22:53:34 +1300

    Implement decideWinner method in GameEngine to determine game outcomes and update tests

--- src/application/GameEngine.js
+++ src/application/GameEngine.js
@@ -15,6 +15,18 @@ class GameEngine {
     this.deck.shuffle();
     // TODO: deal initial hands, set up state, etc.
   }
+
+  decideWinner(playerCard, computerCard) {
+  const RANK_ORDER = ["2","3","4","5","6","7","8","9","10","J","Q","K","A"];
+
+  const playerValue = RANK_ORDER.indexOf(playerCard.rank);
+  const computerValue = RANK_ORDER.indexOf(computerCard.rank);
+
+  if (playerValue > computerValue) return "player";
+  if (computerValue > playerValue) return "computer";
+  return "draw";
+}
+
 }
 
 module.exports = GameEngine;
